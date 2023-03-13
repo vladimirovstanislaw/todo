1b20999390700fabe3ca77f6b3825b7dcb451cde
Stas Vladimirov <zstanislaw211@gmail.com>
2023-03-13 20:51:20 +0300

    another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from "./todoList"
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Bob",
+    [new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets")]);
+  get username(): string {
+    return this.list.user;
+  };
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  };
 }
